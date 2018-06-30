0b1ee2f7b58ebf6dcf49d854bef5ce2ea82f58de
Shashi Kumar <shashikumarlives@gmail.com>
2018-06-30 19:49:31 +0530

    sockets events

--- server/server.js
+++ server/server.js
@@ -1,19 +1,42 @@
 
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
-console.log(__dirname+'/../public');
+console.log(__dirname + '/../public');
 
-const publicPath = path.join(__dirname,'../public');
+const publicPath = path.join(__dirname, '../public');
 
 console.log(publicPath);
 
-
 var app = express();
+// var server = http.createServer((req, res) => {
+// });
+
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
+io.on('connection', function (socket) {
+    console.log("New User Connected ...")
+
+    socket.emit('newemail', {
+        from: ' chandrus server'
+    });
+    socket.on('disconnect', () => {
+        console.log("Client disconnected...");
+    });
+    socket.on('createnewemail', function (mail) {
+        console.log("new email request from client received", mail);
+    });
+});
+
+
+server.listen(3000, () => {
     console.log('Server is up on 3000');
 })
-
+// app.listen(3000, () => {
+//     console.log('Server is up on 3000');
+// })
